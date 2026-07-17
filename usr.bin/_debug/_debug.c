471b4e54996d1b00a87601e6ee50f0102d79bf23
Karina <karina@karter.dev>
2026-07-17 23:16:00 +0400

    feat(_debug): add volatile userspace testing utility

---
+++ usr.bin/_debug/_debug.c
@@ -0,0 +1,10 @@
+// _debug app is meant for me to test things in userspace
+// code of this app is highly volatile as well as app's product
+
+#include <stdio.h>
+
+int
+main(int argc, char *argv[])
+{
+    printf("Hello, World!\n");
+}
